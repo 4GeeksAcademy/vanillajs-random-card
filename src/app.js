5691a3744a66db8112bb9716d1c000e3b9e8b9c9
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-06-30 20:37:34 +0000

    random card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var cardElement = document.getElementById("theCard");
+
+  //possible card values
+  var cardValues = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  // card suits
+  var cardSuits = ["hearts", "spades", "clubs", "diamonds"];
+
+  // generate a random card
+  function randomCard() {
+    // random value index and suit index
+    var valueIndex = Math.floor(Math.random() * cardValues.length);
+    var suitIndex = Math.floor(Math.random() * cardSuits.length);
+
+    // Get the random card value and suit
+    var cardValue = cardValues[valueIndex];
+    var cardSuit = cardSuits[suitIndex];
+
+    // Update the card element with the new value and suit
+    cardElement.className = "card suit-" + cardSuit;
+    cardElement.querySelector("#cardContent").textContent = cardValue;
+  }
+
+  // call main function
+  randomCard();
 };
